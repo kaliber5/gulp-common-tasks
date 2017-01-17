9780f74ecdd4bf90d8dcee8edb932dbb98ba1972
andreasschacht <schacht@kaliber5.de>
2017-01-17 01:45:52 +0100

    add possibility to inject custom options in behat task

--- php/test-behat.js
+++ php/test-behat.js
@@ -1,13 +1,13 @@
-function phpTestBehat(gulp) {
+function phpTestBehat(gulp, options) {
   return function(cb) {
     var behat = require('gulp-behat');
     var gutil = require('gulp-util');
-    
-    var options = { format: 'junit', out: 'build', notify: true };
+
+    options = options || { format: 'junit', out: 'build', notify: true };
     gulp.src('behat.yml')
-      .pipe(behat('bin/behat', options))
-      .on('error', gutil.log)
-      .on('end', cb);
+    .pipe(behat('bin/behat', options))
+    .on('error', gutil.log)
+    .on('end', cb);
   }
 }
 
