57d6e1c2d1410be60c8e5ce7a59df6a9526ed344
simon <ihmig@kaliber5.de>
2017-02-10 11:09:22 +0100

    Add ember fastboot:test support

---
+++ ember/test-fastboot.js
@@ -0,0 +1,31 @@
+function emberTestAll(gulp, options) {
+  return function(cb) {
+    var exec = require('child_process').exec,
+      fs = require('fs'),
+      path = require('path'),
+      outFile = path.join(process.cwd(), 'build/test-fastboot.xml');
+
+    exec('ember fastboot:test --silent --reporter xunit', { maxBuffer: 1024 * 1024 }, function(err, stdout) {
+      fs.writeFile(outFile, stdout, function(fileErr) {
+        if (fileErr) {
+          cb(fileErr);
+          return;
+        }
+        if (!err) {
+          cb();
+          return;
+        }
+        if (err.code === 1) {
+          console.error('Running ember fastboot:test finished with failed tests!');
+          // still return exit code of 0 to continue task processing in CI server
+          cb();
+          return;
+        }
+        cb(err);
+      });
+    });
+  }
+}
+
+module.exports = emberTestAll;
+
